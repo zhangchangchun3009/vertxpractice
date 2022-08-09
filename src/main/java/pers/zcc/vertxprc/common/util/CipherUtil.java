cd3e6cb3a0d8fe44ae4dcf4aeacb9974e91a3523
zhangchangchun <zhangchangchun3009@163.com>
2022-08-09 13:54:14 +0800

    rename vars in CipherUtil

--- src/main/java/pers/zcc/vertxprc/common/util/CipherUtil.java
+++ src/main/java/pers/zcc/vertxprc/common/util/CipherUtil.java
@@ -11,14 +11,16 @@ import javax.crypto.spec.PBEParameterSpec;
 public class CipherUtil {
 
     private static final int ITERATE_COUNT = 1000;
+
     private static final String PBE_WITH_MD5_AND_DES = "PBEWithMD5AndDES";
-    private static final byte[] NUMBERS = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+    private static final byte[] SALT = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
 
     public static String encryptByPBEWithMD5AndDES(String content, String key) throws Exception {
         Cipher cipher = Cipher.getInstance(PBE_WITH_MD5_AND_DES);
         SecretKey secKey = SecretKeyFactory.getInstance(PBE_WITH_MD5_AND_DES)
                 .generateSecret(new PBEKeySpec(key.toCharArray()));
-        PBEParameterSpec parameterspec = new PBEParameterSpec(NUMBERS, ITERATE_COUNT);
+        PBEParameterSpec parameterspec = new PBEParameterSpec(SALT, ITERATE_COUNT);
         cipher.init(Cipher.ENCRYPT_MODE, secKey, parameterspec);
         byte[] results = cipher.doFinal(content.getBytes());
         return new String(Base64.getEncoder().encode(results));
@@ -29,7 +31,7 @@ public class CipherUtil {
         Cipher cipher = Cipher.getInstance(PBE_WITH_MD5_AND_DES);
         SecretKey secKey = SecretKeyFactory.getInstance(PBE_WITH_MD5_AND_DES)
                 .generateSecret(new PBEKeySpec(key.toCharArray()));
-        PBEParameterSpec parameterspec = new PBEParameterSpec(NUMBERS, 1000);
+        PBEParameterSpec parameterspec = new PBEParameterSpec(SALT, ITERATE_COUNT);
         cipher.init(Cipher.DECRYPT_MODE, secKey, parameterspec);
         byte[] res = cipher.doFinal(Base64.getDecoder().decode(content.getBytes()));
         return new String(res);
